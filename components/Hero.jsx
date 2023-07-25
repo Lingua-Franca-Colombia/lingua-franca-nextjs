0e82b6b9a8d7edc6088f66e4800ac76a605cfa62
Alejandro Torres <72758522+jalejotorresm@users.noreply.github.com>
2023-07-25 16:07:22 +0000

    Lingua Franca versio nextjs

---
+++ components/Hero.jsx
@@ -0,0 +1,77 @@
+import {
+    Stack,
+    Flex,
+    Button,
+    Text,
+    VStack,
+    useBreakpointValue,
+  } from "@chakra-ui/react";
+  import "@fontsource/quicksand"
+  
+  import Link from "next/link";
+
+  // import { Link } from "react-router-dom";
+  
+  export default function WithBackgroundImage() {
+    function handleClick() {
+      const element = document.getElementById("features");
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  
+    return (
+      <Flex
+        w={"full"}
+        h={"100vh"}
+        backgroundImage={
+          "url(https://images.unsplash.com/photo-1546430498-05c7b929830e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80)"
+        }
+        backgroundSize={"cover"}
+        backgroundPosition={"center center"}
+      >
+        <VStack
+          fontFamily={"Quicksand"}
+          w={"full"}
+          justify={"center"}
+          px={useBreakpointValue({ base: 4, md: 8 })}
+          bgGradient={"linear(to-r, blackAlpha.600, transparent)"}
+        >
+          <Stack maxW={"2xl"} align={"flex-start"} spacing={6}>
+            <Text
+              color={"white"}
+              fontWeight={700}
+              lineHeight={1.2}
+              fontSize={useBreakpointValue({ base: "3xl", md: "4xl" })}
+            >
+              ¡Comunícate con el mundo! Aprende idiomas o crea tu sitio web con
+              Lingua Franca.
+            </Text>
+            <Stack direction={"row"}>
+              <Button
+                bg={"brand.200"}
+                rounded={"full"}
+                color={"white"}
+                _hover={{ bg: "green.500" }}
+                onClick={handleClick}
+              >
+                Sobre Nosotros
+              </Button>
+              <Button
+                as={Link}
+                href="https://wa.link/v2fhzb"
+                target="_blank"
+                bg={"whiteAlpha.300"}
+                rounded={"full"}
+                color={"white"}
+                _hover={{ bg: "whiteAlpha.500", textDecoration: "none" }}
+              >
+                Escríbenos
+              </Button>
+            </Stack>
+          </Stack>
+        </VStack>
+      </Flex>
+    );
+  }
+  
---
+++ components/NavBar.jsx
@@ -0,0 +1,60 @@
+import { ReactNode } from "react";
+import {
+  Box,
+  Flex,
+  Avatar,
+  Link,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Image } from "@chakra-ui/react";
+import Logo from "../assets/logo.png"
+
+const NavLink = ({ children }) => (
+  <Link
+    px={2}
+    py={1}
+    rounded={"md"}
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+    href={"#"}
+  >
+    {children}
+  </Link>
+);
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4} py={3}>
+        <Flex h={32} alignItems={"center"} justifyContent={"center"}>
+          <Box>
+            <Image src={Logo} alt="Lingua Franca" />
+          </Box>
+
+          {/* <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+              </Button>
+            </Stack>
+          </Flex> */}
+        </Flex>
+      </Box>
+    </>
+  );
+}
