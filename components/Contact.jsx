10a3aee1c7a10ddea683e1288fde3c7da3a1f893
Alejandro Torres <72758522+jalejotorresm@users.noreply.github.com>
2023-07-25 13:50:12 -0500

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,8 +1,9 @@
 import Script from 'next/script'
+import { Box, Container } from '@chakra-ui/react';
 
 export default function Contact() {
     return (
-        <>
+        <Box as={Container} maxW="7xl" id="contact">
             <iframe data-tally-src="https://tally.so/embed/meQ5Xk?alignLeft=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="310" frameBorder="0" marginHeight="0" marginWidth="0" title="Hablemos!
 "></iframe>
 
@@ -13,6 +14,6 @@ export default function Contact() {
                     Tally.loadEmbeds();
                 }}
             />
-        </>
+        </Box>
         )
 }
